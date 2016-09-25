2a3313b48cc9847e10951f5f14405db2b6b89023
Lawrence Daniels <lawrence.daniels@gmail.com>
2016-09-25 11:52:33 -0700

    npm/moment fix

--- npm/moment/src/main/scala/org/scalajs/nodejs/moment/package.scala
+++ npm/moment/src/main/scala/org/scalajs/nodejs/moment/package.scala
@@ -5,12 +5,12 @@ package org.scalajs.nodejs
   * @author lawrence.daniels@gmail.com
   */
 package object moment {
+  val SUNDAY = 0
   val MONDAY = 1
   val TUESDAY = 2
   val WEDNESDAY = 3
   val THURSDAY = 4
   val FRIDAY = 5
   val SATURDAY = 6
-  val SUNDAY = 7
 
 }

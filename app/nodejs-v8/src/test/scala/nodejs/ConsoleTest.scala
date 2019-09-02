53db3e21e17d6b54be0af9fdc9e42d9d3a732d36
exoego <mogami@exoego.net>
2019-09-03 06:47:23 +0900

    Node.js v8 seems not to throw exception on closed writable

--- app/nodejs-v8/src/test/scala/nodejs/ConsoleTest.scala
+++ app/nodejs-v8/src/test/scala/nodejs/ConsoleTest.scala
@@ -14,26 +14,28 @@ class ConsoleTest extends FunSpec with BeforeAndAfterEach {
     if (Fs.existsSync(logFileName)) Fs.unlinkSync(logFileName)
   }
 
-  describe("NodeJS v10") {
-    it("have constructor(stdout, stderr, ignoreErrors) added in v8.0.0") {
-      val failingWritable = Fs.createWriteStream(logFileName)
-      failingWritable.close(_ => {})
-      val looseConsole = new Console(
-        stdout = failingWritable,
-        stderr = failingWritable,
-        ignoreErrors = true
-      )
-      looseConsole.log("ok")
+  it("have constructor(stdout, stderr, ignoreErrors) added in v8.0.0") {
+    val failingWritable = Fs.createWriteStream(logFileName)
+    failingWritable.close(_ => {})
+    val looseConsole = new Console(
+      stdout = failingWritable,
+      stderr = failingWritable,
+      ignoreErrors = true
+    )
+    looseConsole.log("ok")
+  }
 
-      val strictConsole = new Console(
-        stdout = failingWritable,
-        stderr = failingWritable,
-        ignoreErrors = false
-      )
-      val ex = intercept[JavaScriptException] {
-        strictConsole.log("ok")
-      }
-      assert(ex.getMessage().contains("write after end"))
+  it("should suppor ignoreErrors") {
+    assume(TestEnvironment.isExecutedInNode10OrNewer)
+    val strictConsole = new Console(
+      stdout = failingWritable,
+      stderr = failingWritable,
+      ignoreErrors = false
+    )
+
+    val ex = intercept[JavaScriptException] {
+      strictConsole.log("ok")
     }
+    assert(ex.getMessage().contains("write after end"))
   }
 }

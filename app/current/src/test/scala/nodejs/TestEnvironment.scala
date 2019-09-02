822d0dcb4aa9a3032ca34e4d7c8b0d5c2e23be41
exoego <mogami@exoego.net>
2019-09-03 06:00:58 +0900

    Add TestEnvironment helper to determine runtime nodejs version

---
+++ app/current/src/test/scala/nodejs/TestEnvironment.scala
@@ -0,0 +1,16 @@
+package nodejs
+import io.scalajs.nodejs.buffer.Buffer
+import io.scalajs.nodejs.child_process.ChildProcess
+
+object TestEnvironment {
+
+  private lazy val nodeMajorVersion: Int =
+    ChildProcess.execSync("node -v").asInstanceOf[Buffer].toString.drop(1).takeWhile(_.isDigit).toInt
+
+  def isExecutedInExactNode12: Boolean = nodeMajorVersion == 12
+  def isExecutedInExactNode10: Boolean = nodeMajorVersion == 10
+  def isExecutedInExactNode8: Boolean  = nodeMajorVersion == 8
+
+  def isExecutedInNode12OrNewer: Boolean = nodeMajorVersion >= 12
+  def isExecutedInNode10OrNewer: Boolean = nodeMajorVersion >= 10
+}

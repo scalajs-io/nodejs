61243cdbc92dcd7d77d309931235470fca9351fb
exoego <mogami@exoego.net>
2019-09-24 09:45:08 +0900

    Overhaul timers module

--- app/current/src/main/scala/io/scalajs/nodejs/timers/Immediate.scala
+++ app/current/src/main/scala/io/scalajs/nodejs/timers/Immediate.scala
@@ -1,5 +1,6 @@
 package io.scalajs.nodejs.timers
 
+import com.thoughtworks.enableIf
 import io.scalajs.nodejs.clearImmediate
 
 import scala.scalajs.js
@@ -12,6 +13,14 @@ trait Immediate extends js.Object {
 
   def _onImmediate: js.Function = js.native
 
+  @enableIf(io.scalajs.nodejs.CompilerSwitches.gteNodeJs12)
+  def hasRef(): Boolean = js.native
+
+  @enableIf(io.scalajs.nodejs.CompilerSwitches.gteNodeJs10)
+  def ref(): Immediate = js.native
+
+  @enableIf(io.scalajs.nodejs.CompilerSwitches.gteNodeJs10)
+  def unref(): Immediate = js.native
 }
 
 /**

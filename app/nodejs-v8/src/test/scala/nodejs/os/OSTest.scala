c22a035860827f3c50247e601cc491a187294c7f
exoego <mogami@exoego.net>
2019-09-24 09:03:01 +0900

    Overhaul os module

--- app/nodejs-v8/src/test/scala/nodejs/os/OSTest.scala
+++ app/nodejs-v8/src/test/scala/nodejs/os/OSTest.scala
@@ -1,6 +1,5 @@
 package io.scalajs.nodejs.os
 
-import io.scalajs.JSON
 import io.scalajs.util.ScalaJsHelper._
 import org.scalatest.FunSpec
 
@@ -12,97 +11,82 @@ class OSTest extends FunSpec {
   describe("OS") {
 
     it("supports arch()") {
-      info(s"arch: ${OS.arch()}")
-      assert(isDefined(OS.arch()))
+      assert(OS.arch().nonEmpty)
     }
 
     it("supports constants") {
-      info(s"constants: ${JSON.stringify(OS.constants)}")
       assert(isDefined(OS.constants))
     }
 
     it("supports cpus()") {
       val cpus = OS.cpus()
-      cpus.zipWithIndex foreach {
-        case (cpu, n) =>
-          info(s"cpu$n: ${JSON.stringify(cpu)}")
-      }
       assert(isDefined(cpus))
+      assert(cpus(0).model.nonEmpty)
+      assert(cpus(0).speed > 0)
+      assert(cpus(0).times.user > 0)
     }
 
     it("supports endianness()") {
-      info(s"endianness: ${OS.endianness()}")
-      assert(isDefined(OS.endianness()))
+      assert(OS.endianness() === "BE" || OS.endianness() === "LE")
     }
 
     it("supports EOL") {
-      info(s"EOL: ${OS.EOL}")
-      assert(isDefined(OS.EOL))
+      assert(OS.EOL === "\n" || OS.EOL === "\r\n")
     }
 
     it("supports freemem()") {
-      info(s"freemem: ${OS.freemem()}")
-      assert(isDefined(OS.freemem()))
+      assert(OS.freemem() > 0)
     }
 
     it("supports homedir()") {
-      info(s"homedir: ${OS.homedir()}")
       assert(isDefined(OS.homedir()))
     }
 
     it("supports hostname()") {
-      info(s"hostname: ${OS.hostname()}")
       assert(isDefined(OS.hostname()))
     }
 
     it("supports loadavg()") {
-      info(s"loadavg: ${OS.loadavg()}")
       assert(isDefined(OS.loadavg()))
+      assert(OS.loadavg().nonEmpty)
     }
 
     it("supports networkInterfaces()") {
-      OS.networkInterfaces() foreach {
+      val networkInterfaces = OS.networkInterfaces()
+      assert(isDefined(networkInterfaces))
+      networkInterfaces foreach {
         case (name, iface) =>
-          info(s"$name: ${JSON.stringify(iface)}")
+          assert(name.nonEmpty)
+          assert(iface.nonEmpty)
       }
-      assert(isDefined(OS.networkInterfaces()))
     }
 
     it("supports platform()") {
-      info(s"platform: ${OS.platform()}")
-      assert(isDefined(OS.platform()))
+      assert(OS.platform().nonEmpty)
     }
 
     it("supports release()") {
-      info(s"release: ${OS.release()}")
       assert(isDefined(OS.release()))
     }
 
     it("supports tmpdir()") {
-      info(s"tmpdir: ${OS.tmpdir()}")
       assert(isDefined(OS.tmpdir()))
     }
 
     it("supports totalmem()") {
-      info(s"totalmem: ${OS.totalmem()}")
       assert(isDefined(OS.totalmem()))
     }
 
     it("supports type()") {
-      info(s"type: ${OS.`type`()}")
       assert(isDefined(OS.`type`()))
     }
 
     it("supports uptime()") {
-      info(s"uptime: ${OS.uptime()}")
       assert(isDefined(OS.uptime()))
     }
 
     it("supports userInfo()") {
-      info(s"userInfo: ${JSON.stringify(OS.userInfo())}")
       assert(isDefined(OS.userInfo()))
     }
-
   }
-
 }

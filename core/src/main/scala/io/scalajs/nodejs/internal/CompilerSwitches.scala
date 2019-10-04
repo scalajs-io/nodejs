e77062df39f2b15babcc078ce29751a5961c0d1c
exoego <mogami@exoego.net>
2019-10-04 19:03:34 +0900

    Fix format

--- core/src/main/scala/io/scalajs/nodejs/internal/CompilerSwitches.scala
+++ core/src/main/scala/io/scalajs/nodejs/internal/CompilerSwitches.scala
@@ -11,8 +11,8 @@ object CompilerSwitches {
     predicate(major.toInt, minor.toInt, patch.toInt)
   }
 
-  final val isNodeJs8   = (c: whitebox.Context) => c.settings.exists(compare((major, _, _) => major == 8))
-  final val gteNodeJs8  = (c: whitebox.Context) => c.settings.exists(compare((major, _, _) => major >= 8))
+  final val isNodeJs8  = (c: whitebox.Context) => c.settings.exists(compare((major, _, _) => major == 8))
+  final val gteNodeJs8 = (c: whitebox.Context) => c.settings.exists(compare((major, _, _) => major >= 8))
 
   final val isNodeJs10  = (c: whitebox.Context) => c.settings.exists(compare((major, _, _) => major == 10))
   final val gteNodeJs10 = (c: whitebox.Context) => c.settings.exists(compare((major, _, _) => major >= 10))

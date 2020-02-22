e7b8ac67b70632f5aab3f92cd3262be9d9225518
exoego <mogami@exoego.net>
2020-02-22 16:41:16 +0900

    Overhaul events in dgram module

--- app/current/src/main/scala/io/scalajs/nodejs/dgram/package.scala
+++ app/current/src/main/scala/io/scalajs/nodejs/dgram/package.scala
@@ -1,5 +1,8 @@
 package io.scalajs.nodejs
 
+import com.thoughtworks.enableIf
+import io.scalajs.nodejs.buffer.Buffer
+
 import scala.scalajs.js
 import scala.scalajs.js.typedarray.Uint8Array
 import scala.scalajs.js.|
@@ -8,4 +11,14 @@ package object dgram {
   type StringMessage = String | js.Array[String]
   type Message       = BufferMessage | StringMessage
   type BufferMessage = Uint8Array | js.Array[Uint8Array]
+
+  implicit final class SocketExtension[T <: Socket](private val instance: T) extends AnyVal {
+    @enableIf(io.scalajs.nodejs.internal.CompilerSwitches.gteNodeJs12)
+    @inline def onConnect(handler: () => Any): T = instance.on("connect", handler)
+
+    @inline def onClose(handler: () => Any): T                            = instance.on("close", handler)
+    @inline def onError(handler: (Error) => Any): T                       = instance.on("error", handler)
+    @inline def onListening(handler: () => Any): T                        = instance.on("listening", handler)
+    @inline def onMessage(handler: (Buffer, RemoteAddressInfo) => Any): T = instance.on("message", handler)
+  }
 }

2b21d202c569b9a6ea8dd58ec5056abbdb51d22d
exoego <mogami@exoego.net>
2020-03-24 10:59:05 +0900

    Added tls-related properties
    https://nodejs.org/api/https.html#https_new_agent_options

--- app/current/src/main/scala/io/scalajs/nodejs/https/AgentOptions.scala
+++ app/current/src/main/scala/io/scalajs/nodejs/https/AgentOptions.scala
@@ -1,6 +1,10 @@
 package io.scalajs.nodejs.https
 
+import io.scalajs.nodejs.buffer.Buffer
+import io.scalajs.nodejs.tls
+
 import scala.scalajs.js
+import scala.scalajs.js.|
 
 class AgentOptions(
     var keepAlive: js.UndefOr[Boolean] = js.undefined,
@@ -9,5 +13,20 @@ class AgentOptions(
     var maxFreeSockets: js.UndefOr[Int] = js.undefined,
     var timeout: js.UndefOr[Int] = js.undefined,
     var maxCachedSessions: js.UndefOr[Int],
-    var servername: js.UndefOr[String]
+    var servername: js.UndefOr[String],
+    var ca: js.UndefOr[tls.SecureData] = js.undefined,
+    var cert: js.UndefOr[tls.SecureData] = js.undefined,
+    var ciphers: js.UndefOr[String] = js.undefined,
+    var clientCertEngine: js.UndefOr[String] = js.undefined,
+    var crl: js.UndefOr[tls.SecureData] = js.undefined,
+    var dphram: js.UndefOr[String | Buffer] = js.undefined,
+    var ecdhCurve: js.UndefOr[String] = js.undefined,
+    var honorCihperOrder: js.UndefOr[Boolean] = js.undefined,
+    var key: js.UndefOr[tls.SecureData] = js.undefined,
+    var passphrase: js.UndefOr[String] = js.undefined,
+    var pfx: js.UndefOr[tls.SecureData | js.Array[tls.SecureDataObjectForm]] = js.undefined,
+    var rejectUnauthorized: js.UndefOr[Boolean] = js.undefined,
+    var secureOptions: js.UndefOr[Int] = js.undefined,
+    var secureProtocol: js.UndefOr[String] = js.undefined,
+    var sessionIdContext: js.UndefOr[String] = js.undefined
 ) extends js.Object
